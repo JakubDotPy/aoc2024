53bce837f36ebdde2a9f8576ddbdae2613a35a3c
Jakub Červinka <jakub.cervinka@prusa3d.cz>
2024-12-28 16:58:51 +0100

    added submit solution script
    based on asottile

--- support-src/support.py
+++ support-src/support.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import argparse
 import contextlib
 import enum
 import io
@@ -62,6 +63,52 @@ def get_year_day() -> tuple[int, int]:
         raise AssertionError(err_msg)
 
     return int(year_s[len('aoc') :]), int(day_s[len('day') :])
+
+
+TOO_QUICK = re.compile('You gave an answer too recently.*to wait.')
+WRONG = re.compile(r"That's not the right answer.*?\.")
+RIGHT = "That's the right answer!"
+ALREADY_DONE = re.compile(r"You don't seem to be solving.*\?")
+
+
+def _post_answer(year: int, day: int, part: int, answer: int) -> str:
+    params = urllib.parse.urlencode({'level': part, 'answer': answer})
+    req = urllib.request.Request(
+        f'https://adventofcode.com/{year}/day/{day}/answer',
+        method='POST',
+        data=params.encode(),
+        headers=_get_cookie_headers(),
+    )
+    resp = urllib.request.urlopen(req)
+
+    return resp.read().decode()
+
+
+def submit_solution() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--part', type=int, required=True)
+    args = parser.parse_args()
+
+    year, day = get_year_day()
+    answer = int(sys.stdin.read())
+
+    print(f'answer: {answer}')
+
+    contents = _post_answer(year, day, args.part, answer)
+
+    for error_regex in (WRONG, TOO_QUICK, ALREADY_DONE):
+        error_match = error_regex.search(contents)
+        if error_match:
+            print(f'\033[41m{error_match[0]}\033[m')
+            return 1
+
+    if RIGHT in contents:
+        print(f'\033[42m{RIGHT}\033[m')
+        return 0
+    else:
+        # unexpected output?
+        print(contents)
+        return 1
 
 
 def download_input() -> int:
